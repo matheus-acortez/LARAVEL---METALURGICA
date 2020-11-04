bef46b49075d4f2ff1e01dcf04be9b59b601c1ef
matheus-cortez99 <matheus.cortez99@gmail.com>
2020-11-04 00:48:30 -0300

    Ajuste

--- metalurgica/app/Produto.php
+++ metalurgica/app/Produto.php
@@ -3,13 +3,12 @@
 namespace App\Models;
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
 
 	protected $fillable = ['name', 'description', 'quantity', 'price', 'imagem'];
+	protected $guarded = ['id', 'created_at', 'update_at'];
 	protected $table = 'produtos';
-    use HasFactory;
 }
--- metalurgica/resources/views/include-produto.blade.php
+++ metalurgica/resources/views/include-produto.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Comercial Laravel</title>
+        <title>Metalurgica - Laravel</title>
 
         <!-- Favicon -->
         <link href="{{URL::asset('img/favicon.ico')}}" rel="shortcut icon">
