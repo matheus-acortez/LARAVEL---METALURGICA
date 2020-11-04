4bdbc83884420570680e08d298fe1c141a2c23e9
matheus-cortez99 <matheus.cortez99@gmail.com>
2020-11-04 00:12:58 -0300

    Migrações, model e controller para Produtos

---
+++ metalurgica/app/Produto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+
+	protected $fillable = ['name', 'description', 'quantity', 'price', 'imagem'];
+	protected $guarded = ['id', 'created_at', 'update_at'];
+	protected $table = 'produtos';
+    use HasFactory;
+}
