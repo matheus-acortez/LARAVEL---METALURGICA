6decd113c26655bfe0fdbcce145183ba55d6bec7
matheus-cortez99 <matheus.cortez99@gmail.com>
2020-11-29 22:29:04 -0300

    Função logout e view de registro

--- metalurgica/resources/views/auth/register.blade.php
+++ metalurgica/resources/views/auth/register.blade.php
@@ -1,18 +1,65 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head> 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" />       
+
+        <!-- JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="{{URL::asset('js/ajax.js')}}"></script>        
+    <title></title>
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">       
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{url('/')}}" 
+                   title="Página Inicial" style="margin-top: -3px">
+                    <img src="{{URL::asset('img/logo.png')}}"></a>
+                <button type="button" class="navbar-toggle" 
+                        data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>               
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav" id="link-white">
+                    <li>
+                        <a href="#" style="text-decoration: none">
+                            <span class="glyphicon glyphicon-home"></span> 
+                            <span id="underline">Home</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right" id="link-white">
+                    <li class="dropdown">
+                    </li>
+                    <li><a href="#" 
+                           style="text-decoration: none">
+                            <span class="glyphicon glyphicon-log-in"></span> 
+                            <span id="underline">Sair</span></a></li>  
+                    <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                </ul>
+            </div>       
+        </nav>  
+
+    <div class="container" style="margin-top: 100px; margin-left: 440px;">
+    <div class="d-flex justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-3 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +73,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -40,7 +87,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-3 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -54,14 +101,14 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-3 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="endereco" class="col-md-4 col-form-label text-md-right">{{ __('Endereço') }}</label>
+                            <label for="endereco" class="col-md-3 col-form-label text-md-right">{{ __('Endereço') }}</label>
 
                             <div class="col-md-6">
                                 <input id="endereco" type="text" class="form-control @error('endereco') is-invalid @enderror" name="endereco" value="{{ old('endereco') }}" required autocomplete="endereco">
@@ -74,7 +121,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="numero" class="col-md-4 col-form-label text-md-right">{{ __('Número') }}</label>
+                            <label for="numero" class="col-md-3 col-form-label text-md-right">{{ __('Número') }}</label>
 
                             <div class="col-md-6">
                                 <input id="numero" type="text" class="form-control @error('numero') is-invalid @enderror" name="numero" value="{{ old('numero') }}" required autocomplete="numero">
@@ -87,7 +134,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="cidade" class="col-md-4 col-form-label text-md-right">{{ __('Cidade') }}</label>
+                            <label for="cidade" class="col-md-3 col-form-label text-md-right">{{ __('Cidade') }}</label>
 
                             <div class="col-md-6">
                                 <input id="cidade" type="text" class="form-control @error('cidade') is-invalid @enderror" name="cidade" value="{{ old('cidade') }}" required autocomplete="cidade">
@@ -100,7 +147,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="estado" class="col-md-4 col-form-label text-md-right">{{ __('Estado') }}</label>
+                            <label for="estado" class="col-md-3 col-form-label text-md-right">{{ __('Estado') }}</label>
 
                             <div class="col-md-6">
                                 <input id="estado" type="text" class="form-control @error('estado') is-invalid @enderror" name="estado" value="{{ old('estado') }}" required autocomplete="estado">
@@ -113,7 +160,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="cpf" class="col-md-4 col-form-label text-md-right">{{ __('CPF') }}</label>
+                            <label for="cpf" class="col-md-3 col-form-label text-md-right">{{ __('CPF') }}</label>
 
                             <div class="col-md-6">
                                 <input id="cpf" type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" value="{{ old('cpf') }}" required autocomplete="cpf">
@@ -126,7 +173,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="telefone" class="col-md-4 col-form-label text-md-right">{{ __('Telefone') }}</label>
+                            <label for="telefone" class="col-md-3 col-form-label text-md-right">{{ __('Telefone') }}</label>
 
                             <div class="col-md-6">
                                 <input id="telefone" type="text" class="form-control @error('telefone') is-invalid @enderror" name="telefone" value="{{ old('telefone') }}" required autocomplete="telefone">
@@ -153,4 +200,7 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+</body>
+</html>
